b76c6e9cb16b6d3b9f392d170e0bfb0e6bff252a
TuanTQ <tquoctua97@gmail.com>
2021-07-29 01:36:46 +0700

    Folder structure

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+Header.propTypes = {
+
+}
+
+function Header(props) {
+    return (
+        <p>
+            Header
+        </p>
+    );
+}
+
+export default Header;
---
+++ src/features/Todo/components/TodoList/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+TodoList.propTypes = {
+
+}
+
+function TodoList() {
+    return (
+        <p>
+            TodoList
+        </p>
+    )
+}
+
+export default TodoList;
