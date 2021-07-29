1d0ddbbd773328ba09d02ed39e7d65d32aad4d27
TuanTQ <tquoctua97@gmail.com>
2021-07-30 01:18:58 +0700

    47 - Redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, NavLink } from 'react-router-dom';
+import { Link, Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
 
@@ -8,19 +8,19 @@ function App() {
     <div className="App">
       Header
       <p>
-        <Link to="/todos">Todo</Link>
-      </p>
-      <p>
-        <Link to="/album">Album</Link>
-      </p>
-      <p>
         <NavLink to="/todos">Todo</NavLink>
       </p>
       <p>
         <NavLink to="/album">Album</NavLink>
       </p>
-      <Route path="/todos" component={TodoFeature} />
-      <Route path="/album" component={AlbumFeature} />
+      <Switch>
+        <Redirect from="/home" to="/" />
+        <Redirect from="/post-list/:postId" to="/posts/:postId" />
+
+        <Route path="/" component={TodoFeature} exact />
+        <Route path="/todos" component={TodoFeature} />
+        <Route path="/album" component={AlbumFeature} />
+      </Switch>
       Footer
     </div>
   );
