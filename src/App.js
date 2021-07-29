a5b85f1b4ad6a62b43ba37a3cfbca671035da007
TuanTQ <tquoctua97@gmail.com>
2021-07-29 23:30:14 +0700

    Add Link & NavLink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Link, Route, NavLink } from 'react-router-dom';
 import TodoFeature from './features/Todo';
 import AlbumFeature from './features/Album';
 
@@ -7,6 +7,18 @@ function App() {
   return (
     <div className="App">
       Header
+      <p>
+        <Link to="/todos">Todo</Link>
+      </p>
+      <p>
+        <Link to="/album">Album</Link>
+      </p>
+      <p>
+        <NavLink to="/todos">Todo</NavLink>
+      </p>
+      <p>
+        <NavLink to="/album">Album</NavLink>
+      </p>
       <Route path="/todos" component={TodoFeature} />
       <Route path="/album" component={AlbumFeature} />
       Footer
