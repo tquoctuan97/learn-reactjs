02b0548d8f40478fbb832fed55f9c579bc536285
TuanTQ <tquoctua97@gmail.com>
2021-07-29 22:30:58 +0700

    add lib react-router-dom & add Route to feature component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
 
 function App() {
-
   return (
     <div className="App">
-      <TodoFeature/>
+      Header
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/album" component={AlbumFeature} />
+      Footer
     </div>
   );
 }
