beb4a0347d59078aaa55673c747c8563d1c2e9cd
TuanTQ <tquoctua97@gmail.com>
2021-07-30 14:42:06 +0700

    57 - categoryApi, productApi

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -2,23 +2,23 @@ import axiosClient from './axiosClient';
 
 const categoryApi = {
   getAll(params) {
-    const url = '/category/';
+    const url = '/products/';
     return axiosClient.get(url, { params });
   },
   get(id) {
-    const url = `/category/${id}`;
+    const url = `/products/${id}`;
     return axiosClient.get(url);
   },
   add(data) {
-    const url = '/category/';
+    const url = '/products/';
     return axiosClient.post(url, data);
   },
   update(data) {
-    const url = '/category/';
+    const url = '/products/';
     return axiosClient.patch(url, data);
   },
   remove(id) {
-    const url = `/category/${id}`;
+    const url = `/products/${id}`;
     return axiosClient.delete(url);
   },
 };
